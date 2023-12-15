b7e9dba4702a157f6b2c87e636cfc16af8a6c7fd
TexelProto <texelproto@protonmail.com>
2023-12-15 22:35:34 +0100

    Updated create_day script input paths

--- scripts/create_day.sh
+++ scripts/create_day.sh
@@ -19,5 +19,5 @@ fi
 cp "_daytemplate.rs" "$1/src/$2.rs"
 echo "Created ./$1/src/$2.rs"
 
-touch "inputs/$1/$2.txt"
-echo "Created ./inputs/$1/$2.txt"
+touch "$1/inputs/$2.txt"
+echo "Created ./$1/inputs/$2.txt"
